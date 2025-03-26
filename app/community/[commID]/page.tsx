cda94e7f31e289c071790c59ef54f0acfe3f0b36
GRIdiscrete <kjaure@gorillaresearch.net>
2025-03-26 11:26:03 +0200

    l

--- app/community/[commID]/page.tsx
+++ app/community/[commID]/page.tsx
@@ -10,101 +10,101 @@ import Editor from "@/components/editor";
 import { MapProvider } from "@/providers/map-provider";
 import { MapComponent2 } from "@/components/map2";
 
-const Community2 = async ({ params }: { params: { commID: any } }) => {
+const Community2 =  ({ params }: { params: { commID: string } }) => {
     const { commID } =  params;
     // const [map, setMapp] = useState<mapItem[]>([])
-    const getData2 = async () => {
-        const headers = {
-            'x-api-key': 'proTract22$'
-        };
-        const params = {
-            'community_id': commID,
-            // Add more parameters as needed
-        };
+    // const getData2 = async () => {
+    //     const headers = {
+    //         'x-api-key': 'proTract22$'
+    //     };
+    //     const params = {
+    //         'community_id': commID,
+    //         // Add more parameters as needed
+    //     };
 
-        try {
-            const response = await axios.get('https://accounts.protracc.com/api/members', {
-                headers,
-                params
-            });
+    //     try {
+    //         const response = await axios.get('https://accounts.protracc.com/api/members', {
+    //             headers,
+    //             params
+    //         });
 
-            // Save the data to the state variable
-            return response.data.data as Member[]
+    //         // Save the data to the state variable
+    //         return response.data.data as Member[]
 
             
 
 
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    }
+    //     } catch (error) {
+    //         console.error('Error fetching data:', error);
+    //     }
+    // }
 
 
-    const getUniqueOrgIds = (dataArray: Member[]): string[] => {
-        const orgIds = dataArray.map(item => item.org_id);
-        return Array.from(new Set(orgIds));
-      };
+    // const getUniqueOrgIds = (dataArray: Member[]): string[] => {
+    //     const orgIds = dataArray.map(item => item.org_id);
+    //     return Array.from(new Set(orgIds));
+    //   };
       
-      // Usage
+    //   // Usage
 
-    // await getData()
-    const response = await getData2()
+    // // await getData()
+    // const response = await getData2()
 
 
-    const getData3 = async () => {
-        if (response) {
-            const uniqueOrgIds = getUniqueOrgIds(response);
+    // const getData3 = async () => {
+    //     if (response) {
+    //         const uniqueOrgIds = getUniqueOrgIds(response);
 
     
-            // Initialize the map array
-            let map: mapItem[] = [];
+    //         // Initialize the map array
+    //         let map: mapItem[] = [];
     
-            // Iterate through uniqueOrgIds and fetch data
-            for (const x of uniqueOrgIds) { // Use 'of' instead of 'in' to iterate over array values
-                try {
-                    const response4 = await axios.get('https://accounts.protracc.com/api/getAssets', {
-                        headers: {
-                            'x-api-key': 'proTract22$'
-                        },
-                        params: {
-                            'org_id': x
-                        }
-                    });
+    //         // Iterate through uniqueOrgIds and fetch data
+    //         for (const x of uniqueOrgIds) { // Use 'of' instead of 'in' to iterate over array values
+    //             try {
+    //                 const response4 = await axios.get('https://accounts.protracc.com/api/getAssets', {
+    //                     headers: {
+    //                         'x-api-key': 'proTract22$'
+    //                     },
+    //                     params: {
+    //                         'org_id': x
+    //                     }
+    //                 });
     
-                    // Append the result to map
-                    const result = response4.data.data as mapItem[];
-                    console.log(result)
-                    map = [...map, ...result];
-                } catch (error) {
-                    console.error(`Error fetching data for org_id ${x}:`, error);
-                }
-            }
+    //                 // Append the result to map
+    //                 const result = response4.data.data as mapItem[];
+    //                 console.log(result)
+    //                 map = [...map, ...result];
+    //             } catch (error) {
+    //                 console.error(`Error fetching data for org_id ${x}:`, error);
+    //             }
+    //         }
     
-            // Return the map array after all iterations
-            return map;
-        }
+    //         // Return the map array after all iterations
+    //         return map;
+    //     }
     
-        return []; // Return an empty array if response is undefined
-    };
+    //     return []; // Return an empty array if response is undefined
+    // };
     
     // Usage Example
 
-    const map = await getData3();
-    console.log("Final Map Array:", map);
+    // const map = await getData3();
+    // console.log("Final Map Array:", map);
 
 
 
     return (
         <><div className="flex h-screen">
             <Nav comm={commID}/>
-            <div className="flex-1 p-4 overflow-y-auto h-full">
+            {/* <div className="flex-1 p-4 overflow-y-auto h-full">
                 <MapProvider>
                     <MapComponent2 data={map}/>
                 </MapProvider>
                <Editor comm={commID}/>
                 <CommPage comm={commID}/>
                 <Members members={response}/>
-            </div>
+            </div> */}
 
         </div>
         </>
